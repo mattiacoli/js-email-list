cf68e8dc26cb31e3d869d4d6c425252325e5ebe4
mattiacoli <mattia.coli@aol.com>
2025-02-27 16:28:10 +0100

    add event listener to button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // node dom element
 const listEl = document.querySelector('.user_mail_list')
+const buttonEl = document.querySelector('button')
 
 // get 10 random mail using for loop 
 
@@ -23,5 +24,35 @@ for (let i = 0; i < 10; i++) {
   
   
 }
+
+// add event listener to button
+
+buttonEl.addEventListener('click', ()=>{
+
+  // reset list
+  listEl.innerHTML = " "
+  
+  // get 10 random mails
+  for (let i = 0; i < 10; i++) {  
+  
+  // using fetch and Boolean API
+  fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(response=>response.json())
+    .then(data =>{
+      // log data
+      console.log(data.response);
+      
+      // save data in a constant
+      const mail = data.response
+
+      // add mail in dom using Web API
+      listEl.insertAdjacentHTML("beforeend", `<li class="list-group-item" >${mail}</li>`)
+
+
+    })     
+  }
+})
+
+
 
     
