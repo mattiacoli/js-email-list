b8b66e4b986f51e2389f25a6dc0242bc6a91a9d0
mattiacoli <mattia.coli@aol.com>
2025-02-27 16:01:03 +0100

    add 10 randoms mail in a list

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 // node dom element
-
 const listEl = document.querySelector('.user_mail_list')
+
+// get 10 random mail using for loop 
+
+for (let i = 0; i < 10; i++) {  
+  
+  // using fetch and Boolean API
+  fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(response=>response.json())
+    .then(data =>{
+      // log data
+      console.log(data.response);
+      
+      // save data in a constant
+      const mail = data.response
+
+      // add mail in dom using Web API
+      listEl.insertAdjacentHTML("beforeend", `<li>${mail}</li>`)
+
+
+    })   
+  
+  
+}
+
+    
